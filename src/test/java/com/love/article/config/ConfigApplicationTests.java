758a6060af4c81f1c5a294bf59ff663842f0f217
fedexu <federico.xu125@gmail.com>
2020-02-19 19:34:21 +0100

    refactored love-article with submodules

---
+++ src/test/java/com/love/article/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.article.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
